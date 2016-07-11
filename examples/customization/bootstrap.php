e0bcfbd8843d379b0b12f4310b742b253eca1d1b
Tonya <hellofromtonya@thewpdc.com>
2016-07-11 18:54:09 -0500

    including an example customization folder

---
+++ examples/customization/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Customization bootstrap file
+ *
+ * This file serves as the bootstrap (launching point) for all customization to this theme.
+ * You can think of this file as your functions.php for customization.  You want to make your
+ * changes here, include any customization files, etc.
+ *
+ * Do not make any changes outside of the customization folder, as your changes will be lost
+ * when the theme is updated.  However, everything in this folder will be retained upon
+ * theme updates.  WooHoo, yes, you heard me right!
+ *
+ * @package     ${NAMESPACE}
+ * @since       1.0.0
+ * @author      hellofromTonya
+ * @link        https://hellofromtonya.com
+ * @license     GNU General Public License 2.0+
+ */
